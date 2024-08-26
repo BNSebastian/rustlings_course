743bb2c3680bfb4253c7bdbc6541c3e8821603d9
BNSebastian <bucovala.sebastian@protonmail.com>
2024-08-27 01:29:58 +0300

    did them right away

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,10 +10,18 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    if apples > 40 {
+        apples
+    } else {
+        apples * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
+    println!("apples cost {} rust bucks", calculate_price_of_apples(42));
+    println!("apples cost {} rust bucks", calculate_price_of_apples(2));
 }
 
 // Don't change the tests!
